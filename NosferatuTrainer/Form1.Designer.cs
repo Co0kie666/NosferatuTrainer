67da44610804fe12f9f65ff79389f1d2a20c7d3d
Jannick Born <Jannick-born@t-online.de>
2022-03-28 11:12:33 +0200

    Add project files.

---
+++ NosferatuTrainer/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace NosferatuTrainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(130, 96);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Unlimited Ammo";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 198);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Nosferatu Trainer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ NosferatuTrainer/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using System.Threading;
+
+namespace NosferatuTrainer
+{
+    public partial class Form1 : Form
+    {
+        Mem mem = new Mem();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            int PID = mem.GetProcIdFromName("Nosferatu");
+
+            if (PID > 0)
+            {
+                mem.OpenProcess(PID);
+
+                Thread TH = new Thread(writeMemory);
+                TH.Start();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void writeMemory()
+        {
+            while (true)
+            {
+                if (this.checkBox1.Checked)
+                {
+                    // "Nosferatu.exe"+0014A1EC
+                    mem.WriteMemory("Nosferatu.exe+0x0014A1EC,40,14C,158,8,35C,310","int","9999"); // Set loaded pistol ammo to 9999
+                }
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
