f36ce41c653a1145d09689cf7d85f5c24ef79469
Jannick Born <Jannick-born@t-online.de>
2022-03-29 12:39:34 +0200

    Added Unlimited Stamina option

--- NosferatuTrainer/Form1.Designer.cs
+++ NosferatuTrainer/Form1.Designer.cs
@@ -32,23 +32,31 @@
             this.checkBoxHealth = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxEnableTrainer = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBoxStamina = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // checkBoxAmmo
             // 
             this.checkBoxAmmo.AutoSize = true;
-            this.checkBoxAmmo.Location = new System.Drawing.Point(77, 28);
+            this.checkBoxAmmo.Checked = true;
+            this.checkBoxAmmo.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAmmo.Location = new System.Drawing.Point(77, 65);
             this.checkBoxAmmo.Name = "checkBoxAmmo";
             this.checkBoxAmmo.Size = new System.Drawing.Size(101, 17);
             this.checkBoxAmmo.TabIndex = 0;
             this.checkBoxAmmo.Text = "Unlimited Ammo";
             this.checkBoxAmmo.UseVisualStyleBackColor = true;
-            this.checkBoxAmmo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.checkBoxAmmo_KeyDown);
+            this.checkBoxAmmo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.checkBoxes_KeyDown);
             // 
             // checkBoxHealth
             // 
             this.checkBoxHealth.AutoSize = true;
-            this.checkBoxHealth.Location = new System.Drawing.Point(77, 56);
+            this.checkBoxHealth.Checked = true;
+            this.checkBoxHealth.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxHealth.Location = new System.Drawing.Point(77, 93);
             this.checkBoxHealth.Name = "checkBoxHealth";
             this.checkBoxHealth.Size = new System.Drawing.Size(103, 17);
             this.checkBoxHealth.TabIndex = 1;
@@ -58,26 +66,70 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Location = new System.Drawing.Point(12, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Numpad 8:";
+            this.label1.Text = "Numpad 7:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Location = new System.Drawing.Point(12, 94);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Numpad 9:";
+            this.label2.Text = "Numpad 8:";
+            // 
+            // checkBoxEnableTrainer
+            // 
+            this.checkBoxEnableTrainer.AutoSize = true;
+            this.checkBoxEnableTrainer.Location = new System.Drawing.Point(77, 12);
+            this.checkBoxEnableTrainer.Name = "checkBoxEnableTrainer";
+            this.checkBoxEnableTrainer.Size = new System.Drawing.Size(95, 17);
+            this.checkBoxEnableTrainer.TabIndex = 5;
+            this.checkBoxEnableTrainer.Text = "Enable Trainer";
+            this.checkBoxEnableTrainer.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Numpad 0:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Numpad 9:";
+            // 
+            // checkBoxStamina
+            // 
+            this.checkBoxStamina.AutoSize = true;
+            this.checkBoxStamina.Checked = true;
+            this.checkBoxStamina.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxStamina.Location = new System.Drawing.Point(77, 119);
+            this.checkBoxStamina.Name = "checkBoxStamina";
+            this.checkBoxStamina.Size = new System.Drawing.Size(110, 17);
+            this.checkBoxStamina.TabIndex = 8;
+            this.checkBoxStamina.Text = "Unlimited Stamina";
+            this.checkBoxStamina.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(387, 198);
+            this.Controls.Add(this.checkBoxStamina);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBoxEnableTrainer);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBoxHealth);
@@ -96,6 +148,10 @@
         private System.Windows.Forms.CheckBox checkBoxHealth;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxEnableTrainer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxStamina;
     }
 }
 
--- NosferatuTrainer/Form1.cs
+++ NosferatuTrainer/Form1.cs
@@ -15,7 +15,7 @@ namespace NosferatuTrainer
     public partial class MainWindow : Form
     {
         Mem mem = new Mem();
-
+        int initialAmmo;
 
         public MainWindow()
         {
@@ -43,31 +43,67 @@ namespace NosferatuTrainer
 
         private void writeMemory()
         {
+            
+            //this.initialAmmo = mem.ReadInt("Nosferatu.exe+0x0014A1EC,40,14C,158,8,35C,310");
+            //MessageBox.Show(initialAmmo.ToString());
+
+            if (!this.checkBoxAmmo.Checked)
+            {
+                mem.WriteMemory("Nosferatu.exe+0x0014A1EC,40,14C,158,8,35C,310", "int", this.initialAmmo.ToString());
+            }
+
+         
             while (true)
             {
-                if (this.checkBoxAmmo.Checked)
-                {
-                    // "Nosferatu.exe"+0014A1EC
-                    mem.WriteMemory("Nosferatu.exe+0x0014A1EC,40,14C,158,8,35C,310","int","9999"); // Set all ammo to 9999
+                if (this.checkBoxEnableTrainer.Checked)
+                {              
+                    if (this.checkBoxAmmo.Checked)
+                    {
+                        // "Nosferatu.exe"+0014A1EC
+
+                        string ammoValue = "9999";
+                        mem.WriteMemory("Nosferatu.exe+0x0014A1EC,40,14C,158,8,35C,310", "int", ammoValue); // Set all ammo to 9999 
+                    }
+
+                    if (this.checkBoxHealth.Checked)
+                    {
+                        // "Nosferatu.exe"+0014A1EC
+                        mem.WriteMemory("Nosferatu.exe+0x0014A1EC,D68,14C,398,158,4,14C,3C0", "float", "100"); // Set health to a permanent 100
+                    }
+
+                    if (this.checkBoxStamina.Checked)
+                    {
+                        // "Nosferatu.exe"+0014C09C
+                        mem.WriteMemory("Nosferatu.exe+0x0014C09C,A0,1D4,38,14,38,3B0", "float", "1"); // Set Stamina to a permanent 1 (No exhaustion)
+                    }
+
+                    Thread.Sleep(50);
                 }
-                if (this.checkBoxHealth.Checked)
-                {
-                    // "Nosferatu.exe"+0014A1EC
-                    mem.WriteMemory("Nosferatu.exe+0x0014A1EC,D68,14C,398,158,4,14C,3C0", "float", "100"); // Set health to a permanent 100
-                }
-                Thread.Sleep(50);
             }
         }
 
-        private void checkBoxAmmo_KeyDown(object sender, KeyEventArgs e)
+        private bool isTrainerEnabled()
         {
-            if (e.KeyCode == Keys.NumPad8)
+            return this.checkBoxEnableTrainer.Checked;
+        }
+
+        private void checkBoxes_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.NumPad0)
+            {
+                this.checkBoxEnableTrainer.Checked = !this.checkBoxEnableTrainer.Checked;
+            }
+            if (e.KeyCode == Keys.NumPad7)
             {
                 this.checkBoxAmmo.Checked = !this.checkBoxAmmo.Checked;
             }
-            if (e.KeyCode == Keys.NumPad9)
+            if (e.KeyCode == Keys.NumPad8)
             {
                 this.checkBoxHealth.Checked = !this.checkBoxHealth.Checked;
+            }
+            if (e.KeyCode == Keys.NumPad9)
+            {
+                this.checkBoxStamina.Checked = !this.checkBoxStamina.Checked;
             }
         }
     }
